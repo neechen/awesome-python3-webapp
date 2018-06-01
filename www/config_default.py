7b064c112c22d3e9a3795f6097448f275eaf5b70
倪晨皓 <nee2918@126.com>
2018-06-01 16:57:45 +0800

    blog

---
+++ www/config_default.py
@@ -0,0 +1,15 @@
+# Default configurations.
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': 'nichenhao2918',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,7 @@
+# Override configurations.
+
+configs = {
+    'db': {
+        'host': '127.0.0.1'
+    }
+}
